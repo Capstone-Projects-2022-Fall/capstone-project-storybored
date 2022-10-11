942dd949d9486017e6c0cb6ded7a5a2b5638c47e
Christine Cho <tun66463@temple.edu>
2022-10-11 19:42:29 -0400

    frontend/backend work

--- storybored-react/src/App.js
+++ storybored-react/src/App.js
@@ -1,28 +1,125 @@
-import logo from './logo.svg';
-import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+import React, { useEffect, useState } from 'react'
+
+import { Stage, Layer, Line, Text } from 'react-konva';
 
 
-const
+
+export const onPeerData = (id, data) => {
+    // let msg = JSON.parse(data);
+    console.log(id,data, "receiving")
+    // if (msg.event === 'draw') {
+    //     draw(msg);
+    // } else if (msg.event === 'drawRect') {
+    //     drawRect(msg);
+    // } else if (msg.event === 'clear') {
+    //     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // }
+}
+
+const Canvas = (props) => {
+
+    const [tool, setTool] = React.useState('pen');
+    const [lines, setLines] = React.useState([]);
+    const isDrawing = React.useRef(false);
+    
+    const handleMouseDown = (e) => {
+        isDrawing.current = true;
+        const pos = e.target.getStage().getPointerPosition();
+        
+        setLines([...lines, { tool, points: [pos.x, pos.y] }]);
+        props.broadcast([...lines, { tool, points: [pos.x, pos.y] }]);
+    };
+
+    const handleMouseMove = (e) => {
+        // no drawing - skipping
+        if (!isDrawing.current) {
+            return;
+        }
+        const stage = e.target.getStage();
+        const point = stage.getPointerPosition();
+        let lastLine = lines[lines.length - 1];
+        // add point
+        lastLine.points = lastLine.points.concat([point.x, point.y]);
+
+        // replace last
+        lines.splice(lines.length - 1, 1, lastLine);
+        
+        setLines(lines.concat());
+        props.broadcast(lines.concat());
+    };
+
+    const handleMouseUp = () => {
+        isDrawing.current = false;
+    };
+
+    
+    
+
+    return (
+        <div>
+            <Stage
+                width={window.innerWidth}
+                height={window.innerHeight}
+                onMouseDown={handleMouseDown}
+                onMousemove={handleMouseMove}
+                onMouseup={handleMouseUp}
+            >
+                <Layer>
+                    <Text text="Just start drawing" x={5} y={30} />
+                    {lines.map((line, i) => (
+                        <Line
+                            key={i}
+                            points={line.points}
+                            stroke="#df4b26"
+                            strokeWidth={5}
+                            tension={0.5}
+                            lineCap="round"
+                            lineJoin="round"
+                            globalCompositeOperation={
+                                line.tool === 'eraser' ? 'destination-out' : 'source-over'
+                            }
+                        />
+                    ))}
+                </Layer>
+            </Stage>
+            <select
+                value={tool}
+                onChange={(e) => {
+                    setTool(e.target.value);
+                }}
+            >
+                <option value="pen">Pen</option>
+                <option value="eraser">Eraser</option>
+            </select>
+        </div>
+    );
+};
+
+
+
+
+
+// var shape_map = new Map();
+// const rtcConfig = {
+//     iceServers: [
+//         {
+//             urls: ["stun:stun.l.google.com:19302", "stun:global.stun.twilio.com:3478"],
+//         },
+//     ],
+// };
+
+const App = (props) => {
+
+    return (
+        <div>
+            <Canvas broadcast={props.broadcast}/>
+        </div>
+
+    )
+}
+
+
+
 
 export default App;
--- storybored-react/src/index.js
+++ storybored-react/src/index.js
@@ -3,11 +3,180 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios'
+import {onPeerData} from './App.js'
+
+
+
+var context = {
+  username: "user" + parseInt(Math.random() * 100000),
+  // roomID: window.location.pathname.substr(1),
+  roomID: '5gsw5',
+  token: null,
+  peers: {},
+  channels: {},
+}
+
+const rtcConfig = {
+  iceServers: [
+    {
+      urls: ["stun:stun.l.google.com:19302", "stun:global.stun.twilio.com:3478"],
+    },
+  ],
+};
+
+async function getToken() {
+  let data = {
+    username: context.username,
+  }
+
+  axios.post('http://localhost:7007/access', data, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+  .then(response => {
+    context.token = response.data.token
+    console.log(context.token)
+    connect(response.data.token)
+    
+  })
+}
+
+async function connect(token) {
+  // await getToken()
+  // was missing this line - need to make EventSource to open persistent connection to server - SSE
+  console.log(context.token)
+  context.eventSource = new EventSource(`http://localhost:7007/connect?token=${token}`);
+
+  context.eventSource.addEventListener("add-peer", addPeer, false);
+  context.eventSource.addEventListener("remove-peer", removePeer, false);
+  context.eventSource.addEventListener("session-description", sesssionDescription, false);
+  context.eventSource.addEventListener("ice-candidate", iceCandidate, false);
+  context.eventSource.addEventListener("connected", (user) => {
+    console.log("CONNECTED")
+    context.user = user;
+    console.log("RESULT", join(token));
+  });
+}
+
+
+// for now, room ID is static, assume that the createRoom function has returned a roomdID
+async function join(token) {
+
+  console.log("t", token)
+  return axios.post(`http://localhost:7007/${context.roomID}/join`, {
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`,
+    }
+  })
+}
+
+function addPeer(data) {
+  let message = JSON.parse(data.data);
+  if (context.peers[message.peer.id]) {
+    return;
+  }
+
+  let peer = new RTCPeerConnection(rtcConfig);
+
+  // was missing this line - need to add new peer to peers list
+  context.peers[message.peer.id] = peer;
+
+  peer.onicecandidate = function (event) {
+    if (event.candidate) {
+      // was missing second parameter 'ice-candidate' in relay function
+      relay(message.peer.id, "ice-candidate", event.candidate);
+    }
+  };
+
+  if (message.offer) {
+    // create the data channel, map peer updates
+    let channel = peer.createDataChannel("updates");
+    channel.onmessage = function (event) {
+      onPeerData(message.peer.id, event.data);
+    };
+    context.channels[message.peer.id] = channel;
+    createOffer(message.peer.id, peer);
+  } else {
+    peer.ondatachannel = function (event) {
+      context.channels[message.peer.id] = event.channel;
+      event.channel.onmessage = function (evt) {
+        onPeerData(message.peer.id, evt.data);
+      };
+    };
+  }
+}
+
+async function createOffer(peerID, peer) {
+  let offer = await peer.createOffer();
+  await peer.setLocalDescription(offer);
+  await relay(peerID, "session-description", offer);
+}
+
+function relay(peerID, event, data) {
+  console.log("relaying....", context.token)
+
+  axios.post(`http://localhost:7007/relay/${peerID}/${event}`, data, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${context.token}`,
+    }
+  })
+  
+}
+
+function peerDataUpdate(peerID, data) {
+  onPeerData(peerID, data);
+}
+
+function removePeer(data) {
+  let message = JSON.parse(data.data);
+  // should've been peer.id not user.id 
+  if (context.peers[message.peer.id]) {
+    context.peers[message.peer.id].close();
+  }
+  delete context.peers[message.peer.id];
+}
+
+async function sesssionDescription(data) {
+  let message = JSON.parse(data.data);
+  let peer = context.peers[message.peer.id];
+
+  let remoteDescription = new RTCSessionDescription(message.data);
+
+  await peer.setRemoteDescription(remoteDescription);
+  if (remoteDescription.type === "offer") {
+    let answer = await peer.createAnswer();
+    await peer.setLocalDescription(answer);
+    await relay(message.peer.id, "session-description", answer);
+  }
+}
+
+function iceCandidate(data) {
+  let message = JSON.parse(data.data);
+  let peer = context.peers[message.peer.id];
+  peer.addIceCandidate(new RTCIceCandidate(message.data));
+}
+
+function broadcast(data) {
+  console.log("BROADCASTING", data)
+  for (let peerId in context.channels) {
+      if (context.channels[peerId].readyState === 'open') {
+          context.channels[peerId].send(data);
+      }
+  }
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+getToken();
+
+
 root.render(
   <React.StrictMode>
-    <App />
+    <App broadcast={broadcast}/>
   </React.StrictMode>
 );
 
