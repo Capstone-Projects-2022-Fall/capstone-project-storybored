9e526d035a9d4d8d1620b5666ff489ccde11a23e
Christine Cho <tun66463@temple.edu>
2022-10-15 14:03:27 -0400

    comments

--- storybored-app/storybored-frontend/src/components/Canvas.js
+++ storybored-app/storybored-frontend/src/components/Canvas.js
@@ -14,7 +14,7 @@ const Canvas = ({broadcast, lines, setLines, tool, user}) => {
         // grab position 
         const pos = e.target.getStage().getPointerPosition();
 
-        // current position is the previous lines combined with new point
+        // current position is the previous lines combined with new point information + userid information 
         let currentPoint = [...lines, { tool, points: [ pos.x, pos.y ], user: user}]
 
         // set line as new point
@@ -22,22 +22,34 @@ const Canvas = ({broadcast, lines, setLines, tool, user}) => {
         broadcast(JSON.stringify(currentPoint));
     };
 
+    // the function is triggered IF it passes the first condition, which is the mouse is DOWN 
     const handleMouseMove = (e) => {
         if (!isDrawing.current) {
             return;
         }
+
+        // grab current position
         const pos = e.target.getStage().getPointerPosition();
 
+        // lines is a list of lines (mouse down, mouse movement, mouse up), so grab the index of the last line that was drawn by the CURRENT USER 
         let lastLineIndex = lines.findLastIndex(element => element.user===user)
+
+        // grab the last line with the index 
         let lastLine = lines[lastLineIndex]
 
+        // add the current position of the mouse to the lastLine points (update the line currently being drawn)
         lastLine.points = lastLine.points.concat([ pos.x, pos.y ]);
 
-        // dont want to mutate original variable 
+        // dont want to mutate original variable lines, so make a copy of lines
         let newLine = lines
+        
+        // splice newLine to update lines variable --> use index of the last line drawn by the user and replace with the lastLine variable which has the new points added to it
         newLine.splice(lastLineIndex, 1, lastLine);
 
+        // finally set the lines variables to our updated version
         setLines(newLine.concat());
+
+        // broadcast
         broadcast(JSON.stringify(lines.concat()));        
     };
 
