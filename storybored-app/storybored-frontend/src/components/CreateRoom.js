840e32488abe1d88ba346ed7d9e30a9acfb3bc6e
tun35770 <tun35770@temple.edu>
2022-10-19 19:02:21 -0400

    Branch created

---
+++ storybored-app/storybored-frontend/src/components/CreateRoom.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const CreateRoom = () => {
+
+    const navigate = useNavigate()
+
+    const navToCanvas = () => {
+        navigate('/Canvas')
+    }
+
+  return (
+    <div className='CreateRoom'>
+        <h1>Create Room</h1>
+        
+        <div id='options'>
+            <form>
+                <label for='private-input' id='private-label'>Make Private</label> 
+                <input type='checkbox' id='private-input'></input>
+            </form>
+                {/*Here if Make Private is checked, a password <p></p> will need to be injected dynamically*/}
+            
+            <form>
+                <label for='canvas-size-input' id='canvas-size-label'>Canvas Size</label>
+                <input type='text' id='canvas-size-input'></input>
+            </form>
+        </div>
+        <button id='start-button' onClick={navToCanvas}>Start!</button>
+    </div>
+  )
+}
+
+export default CreateRoom
